2799b8584a8607dc342d610c4ce69d8bcee1f08b
Maryamhizaji <119852045+Maryamhizaji@users.noreply.github.com>
2022-12-25 23:30:56 -0800

    Update text.py

--- text.py
+++ text.py
@@ -2,3 +2,9 @@ print("tish is a test edit project")
 a= 12
 for i in rang(a):
   print(i)
+
+-----------------------------------------------------------
+n=int(input('n= '))
+for i in range(1,n+1)
+print((n-i)*' '+(2*i-1)*'*')
+-------------------------------------------------
